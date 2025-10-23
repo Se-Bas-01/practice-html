e4d7e8e819f4592ab19f2ea26726d563c7a6f204
Sebastien King <sebastienking0511@gmail.com>
2025-10-24 10:09:06 +1100

    Have messed around with strings and const variable

---
+++ Javascript/mathWithJS/strings.js
@@ -0,0 +1,16 @@
+// *** This section is basic string decleration and logging. *** \\
+//creating and logging const variable named string01.
+const string01 = "The revolution will not be televised.";
+console.log(string01)
+
+//creating and logging const variable named badString01.
+const badString01 = string01;
+console.log(badString01);
+
+//creating and logging const variables named single01, double01 backtick01.
+const single01 = 'Single Quotes';
+const double01 = "Double Quotes";
+const backtick01 = `Backticks`;
+console.log(single01);
+console.log(double01);
+console.log(backtick01);
